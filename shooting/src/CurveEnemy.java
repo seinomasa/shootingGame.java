8b36107412aac5d8b28456464a7cc6021db21b07
osamu <kopoandms@gmail.com>
2024-06-18 11:01:19 +0900

    11.5

--- shooting/src/CurveEnemy.java
+++ shooting/src/CurveEnemy.java
@@ -1,4 +1,16 @@
 
-public class CurveEnemy {
+public class CurveEnemy extends Enemy {
+	public CurveEnemy(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
+	}
 
+	public void move() {
+		super.move();
+		if (x < GameWorld.player.x) {
+			x++;
+		}
+		if (x > GameWorld.player.x) {
+			x--;
+		}
+	}
 }
--- shooting/src/EnemyBase.java
+++ shooting/src/EnemyBase.java
@@ -20,5 +20,8 @@ public class EnemyBase extends Enemy {
 		if (Math.random()<0.05) {
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
 		}
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
+		}
 	}
 }
