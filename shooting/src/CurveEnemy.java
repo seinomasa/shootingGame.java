aca94bca621e6360a8a430fd40aa11ba5ea99509
osamu <kopoandms@gmail.com>
2024-06-25 10:57:35 +0900

    13章の練習問題

--- shooting/src/CurveEnemy.java
+++ shooting/src/CurveEnemy.java
@@ -2,16 +2,16 @@
 public class CurveEnemy extends Enemy {
 	public CurveEnemy(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
-		life=3;
+		life=3+GameWorld.stage;
 	}
 
 	public void move() {
 		super.move();
 		if (x < GameWorld.player.x) {
-			x++;
+			x=x+GameWorld.stage;
 		}
 		if (x > GameWorld.player.x) {
-			x--;
+			x=x-+GameWorld.stage;
 		}
 		
 	}
